b4ecaf9b3f28147eefc48d564a18d3171196b60d
kouwa stephane <kksasteph888@gmail.com>
2022-07-23 09:18:43 +0200

    MAJ

--- wbu-atomique-theme/src/js/managepackvhsost.js
+++ wbu-atomique-theme/src/js/managepackvhsost.js
@@ -1,1 +1,2 @@
 import "@stephane888/wbu-atomique/scss/drupal/modules/managepackvhsost/managepackvhsost.scss";
+import "@stephane888/wbu-atomique/scss/drupal/modules/managepackvhsost/managepackvhsost.js";
